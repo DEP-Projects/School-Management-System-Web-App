41c7bdbb2627a6cd3b10a16649e42aad55b0096c
Suchira Liyanage <suchiraliyanage94@gmail.com>
2020-12-02 22:22:12 +0530

    Add import files to courses.component.js

---
+++ src/app/courses/courses.component.js
@@ -0,0 +1,6 @@
+import courses from './courses.component.html';
+import style from './courses.component.scss';
+
+$("app-courses").replaceWith('<div id="courses">' + courses + '</div>');
+var html = '<style>' + style + '</style>';
+$("#courses").append(html);
