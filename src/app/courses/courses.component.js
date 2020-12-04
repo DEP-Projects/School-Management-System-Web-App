3e4a47283a6ee21dde00c7e18a2ec71649538ac9
Suchira Liyanage <suchiraliyanage94@gmail.com>
2020-12-04 10:29:12 +0530

    Implement adding course

--- src/app/courses/courses.component.js
+++ src/app/courses/courses.component.js
@@ -6,5 +6,15 @@ $("app-courses").replaceWith('<div id="courses">' + courses + '</div>');
 var html = '<style>' + style + '</style>';
 $("#courses").append(html);
 
-
-
+$("#btnSave").click(function (){
+    $("#showCourses").append('<div class="info-box">' +
+        '<span class="info-box-icon bg-dark"><i class="fas fa-book-reader"></i></span>'+
+        '<div class="info-box-content">' +
+            '<span class="info-box-text">' + $("#txt-name").val() + '</span>' +
+            '<span class="info-box-number">' + '00' + '</span>'+
+        '</div>' +
+        '<a href="#" class="info-box-footer">' +
+            'More info' + '<i class="fas fa-arrow-circle-right"></i>' +
+        '</a>' +
+    '</div>')
+})
