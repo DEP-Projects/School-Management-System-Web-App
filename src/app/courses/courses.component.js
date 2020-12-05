80589607048b0a4fb0b4ef2bf7dcb603445ff3cf
Suchira Liyanage <suchiraliyanage94@gmail.com>
2020-12-05 08:03:59 +0530

    Add enrollments table

--- src/app/courses/courses.component.js
+++ src/app/courses/courses.component.js
@@ -13,8 +13,21 @@ $("#btnSave").click(function (){
             '<span class="info-box-text">' + $("#txt-name").val() + '</span>' +
             '<span class="info-box-number">' + '00' + '</span>'+
         '</div>' +
-        '<a href="#" class="info-box-footer">' +
-            'More info' + '<i class="fas fa-arrow-circle-right"></i>' +
-        '</a>' +
+        '<div class="info-box-footer">' +
+            '<a href="#">' +
+                'More info ' + '<i class="fas fa-arrow-circle-right"></i>' +
+            '</a>' +
+            '<span class="info-box-text">'+ $("#txt-trainer").val() + '</span>' +
+        '</div>' +
     '</div>')
+})
+
+$("#showCourses>.info-box> .info-box-footer> a").click(function (){
+    $(".content-wrapper").addClass('d-none');
+    $("#courseDetails").removeClass('d-none');
+})
+
+$("#courseDetails .breadcrumb li a").click(function (){
+    $(".content-wrapper").addClass('d-none');
+    $("#coursesMain").removeClass('d-none');
 })
