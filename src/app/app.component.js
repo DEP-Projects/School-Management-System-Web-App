b1392ae41db92858aa978c6dd6434e8f9c0be6bf
Suchira Liyanage <suchiraliyanage94@gmail.com>
2020-12-06 08:51:10 +0530

    import admin-lte js

--- src/app/app.component.js
+++ src/app/app.component.js
@@ -5,6 +5,11 @@ import "../../node_modules/jquery/dist/jquery.min.js";
 import "../../node_modules/bootstrap/dist/js/bootstrap.js";
 import "../../node_modules/bootstrap/dist/js/bootstrap.min.js";
 
+import '../../node_modules/admin-lte/plugins/bootstrap/js/bootstrap.bundle.min.js';
+import '../../node_modules/admin-lte/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js';
+import '../../node_modules/admin-lte/dist/js/adminlte.js';
+
+
 $("app-root").replaceWith('<div id="app">'+app+ '</div>');
 var stylesheet = '<style>' +style + '</style>';
 $('#app').append(stylesheet);
